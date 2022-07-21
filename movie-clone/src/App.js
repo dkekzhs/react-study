29f8313baaacc49bd9c723af61fff05a93741f13
DongJae <dkekzhs5@naver.com>
2022-07-21 20:12:38 +0900

    react-study-projects

---
+++ movie-clone/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import {  Routes, HashRouter, Route } from 'react-router-dom';
+import About from "./routes/About";// 
+import Home from "./routes/Home";
+import Navigation from "./components/NaviBar/Navigation";
+import Detail from "./routes/Detail";
+
+function App(){
+  return (
+    <HashRouter>
+      <Navigation/>
+      <Routes>   
+          <Route path="/" element={<Home/>} ></Route>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/movie-detail" element={<Detail/>}/>
+  
+      </Routes>
+      </HashRouter>
+
+  );
+}
+
+export default App;
---
+++ movie-clone/src/routes/Detail.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Detail(props){
+    console.log(props.title);
+    return(<div>ㅋㅋ</div>);
+}
+export default Detail;
