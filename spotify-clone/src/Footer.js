f173e4e326f10b80701b39e5dca212ae8689e29f
DongJae <dkekzhs5@naver.com>
2022-07-31 01:03:00 +0900

    spotify-clone-music-playing

--- spotify-clone/src/Footer.js
+++ spotify-clone/src/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React ,{ useEffect }from 'react'
 import "./Footer.css"
 
 import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
@@ -11,17 +11,60 @@ import PauseCircleOutlineIcon from '@mui/icons-material/PauseCircleOutline';
 import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay';
 import { Grid, Slider } from "@mui/material/";
 
-function Footer() {
+import { useDataLayerValue } from "./DataLayer";
+
+function Footer({spotify}) {
+  const [{item , playing } ,dispatch] = useDataLayerValue();
+
+  useEffect(() => {
+    spotify.getMyCurrentPlaybackState().then((r) => {
+      dispatch({
+        type: "SET_PLAYING",
+        playing: r.is_playing,
+      });
+
+      dispatch({
+        type: "SET_ITEM",
+        item: r.item,
+      });
+    });
+
+  }, [spotify]);
+
+  
+  const handlePlayPause = () => {
+    if (playing) {
+      spotify.pause();
+      dispatch({
+        type: "SET_PLAYING",
+        playing: false,
+      });
+    } else {
+      spotify.play();
+      dispatch({
+        type: "SET_PLAYING",
+        playing: true,
+      });
+    }
+  };
   return (
     <div className='footer'>
         <div className="footer__left">
-            <h1>앨범 디테일</h1>
-
+          <img className='footer__albumLogo' 
+          src='https://t1.daumcdn.net/cfile/tistory/24283C3858F778CA2E' 
+          alt=''/>
+          <div className='footer__songInfo'>
+            <h4>Yeah</h4>
+            <p>zzzz</p>
+          </div>
         </div>
         <div className="footer__center">
             <ShuffleIcon className='footer__green'/>
             <SkipPreviousIcon className='footer__icon'/>
-            <PlayCircleOutlineIcon fontSize='large'/>
+            {playing?
+            (<PauseCircleOutlineIcon className='footer__icon' fontSize='large' onClick={handlePlayPause}/>):
+            (<PlayCircleOutlineIcon className='footer__icon' fontSize='large' onClick={handlePlayPause}/>)}
+
             <SkipNextIcon className='footer__icon'/>
             <RepeatIcon className='footer__green'/>
         </div>
